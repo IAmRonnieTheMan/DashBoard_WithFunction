acd4e6e520108f9125f556f0af7386b67cf05ddf
IAmRonnieTheMan <david50420yu@gmail.com>
2024-04-25 16:57:59 +0800

    123412356

--- src/server/server.js
+++ src/server/server.js
@@ -4,27 +4,21 @@ const cors = require('cors');
 //解析文件類型,提取上傳文件,將文件保存至指定目錄中
 const multer = require('multer');
 const path = require('path');
-//為node.js模組,提供讀取文件,寫入文件,刪除文件,修改文件
-const fs = require('fs');
-//如果沒有該目錄,利用fs模塊創建一個
-const uploadDir = 'upload';
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir);
-}
 
 //可通過app變量訪問express的各種功能
 const app = express();
 //此為應用express實例
+//更改header
 app.use(cors({
     origin: 'http://localhost:8080', //允許來自該地址的請求
     credentials: true //表示允許認證訊息,如cookies,http認證
 }));
-
 // 設置 Multer 中間件來處理文件上傳
 //multer.diskStorage＝儲存在本機磁
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         //path.join(__dirname加入後就成功執行了
+        //__dirname：取得目前名稱執行程序所在的完整目錄
         cb(null, path.join(__dirname, 'upload/')); // 指定文件保存的目录为当前文件所在目录下的 upload 文件夹
     },
     
@@ -33,8 +27,8 @@ const storage = multer.diskStorage({
     }
 });
 const upload = multer({ storage: storage });
-
 // 處理文件上傳的 POST 請求
+//express定義的post請求路由,當發送post請求時,express會調用路由處理函數
 app.post('/upload', upload.single('file1'), (req, res) => {
     try {
         // 获取上传的文件信息
@@ -43,7 +37,6 @@ app.post('/upload', upload.single('file1'), (req, res) => {
         if (!file) {
             return res.status(400).send('No file uploaded.');
         }
-        
         // 在这里可以进行其他的文件处理操作，例如保存文件信息到数据库
         //200为http协议中定义的状态码
         res.status(200).send('File uploaded successfully.');
@@ -53,9 +46,9 @@ app.post('/upload', upload.single('file1'), (req, res) => {
     }
 });
 
-
 // 監聽端口
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
