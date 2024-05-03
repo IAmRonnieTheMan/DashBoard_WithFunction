447eafbd1f90daa587cc151b85f6fb4f7919b421
IAmRonnieTheMan <david50420yu@gmail.com>
2024-05-03 08:27:00 +0800

    測試firebase中

--- src/server/server.js
+++ src/server/server.js
@@ -16,19 +16,24 @@ app.use(cors({
 // 設置 Multer 中間件來處理文件上傳
 //multer.diskStorage＝儲存在本機磁
 const storage = multer.diskStorage({
+    //cb為回調函數,cb的第一個值為如果出錯時放置的位置,第二個值是儲存路徑
     destination: function (req, file, cb) {
         //path.join(__dirname加入後就成功執行了
         //__dirname：取得目前名稱執行程序所在的完整目錄
-        cb(null, path.join(__dirname, 'upload/')); // 指定文件保存的目录为当前文件所在目录下的 upload 文件夹
+        //path.join為拼接路徑的函式
+        cb(null, path.join(__dirname, 'upload/')); // 指定文件保存的目錄为当前文件所在目錄下的 upload 文件夹
     },
     
     filename: function (req, file, cb) {
         cb(null, file.originalname); // 使用原始文件名作為保存的文件名
     }
 });
+//把multer函式調用給upload使用,multer的storage方式為以上編寫的邏輯
 const upload = multer({ storage: storage });
 // 處理文件上傳的 POST 請求
 //express定義的post請求路由,當發送post請求時,express會調用路由處理函數
+//當客戶端發送post到/upload路徑時執行後續函式
+//.single為只處理單個文件,而且只處理名為file1的文件
 app.post('/upload', upload.single('file1'), (req, res) => {
     try {
         // 获取上传的文件信息
@@ -37,7 +42,7 @@ app.post('/upload', upload.single('file1'), (req, res) => {
         if (!file) {
             return res.status(400).send('No file uploaded.');
         }
-        // 在这里可以进行其他的文件处理操作，例如保存文件信息到数据库
+        //在这里可以进行其他的文件处理操作，例如保存文件信息到数据库
         //200为http协议中定义的状态码
         res.status(200).send('File uploaded successfully.');
     } catch (error) {
--- src/views/AppTest.vue
+++ src/views/AppTest.vue
@@ -28,18 +28,22 @@
    methods: {
      uploadFile() {
        const file = this.$refs.fileInput.files[0];
+       //表單數據,創建後搭配ajax或其他方式上傳
        const formdata = new FormData();
        formdata.append("file1", file);
-      //  ajax是一種頁面不重新加載的情況下,通過後台伺服器進行少量數據交換來更新部份頁面,這種技術通過在後台發送http請求,接受響應並更新頁面內容
+      //ajax是一種頁面不重新加載的情況下,通過後台伺服器進行少量數據交換來更新部份頁面,這種技術通過在後台發送http請求,接受響應並更新頁面內容
        const ajax = new XMLHttpRequest();
-       //當過程中觸發progress事件時,恨調用名為progressHandler的回掉函數（可以在事件發生时值行特定的邏輯）來處理上傳進度的更新
-       //false代表事件監聽將在事件補獲階段之後執行,而不是捕獲前執行
+       //當過程中觸發自訂註冊的progress事件時,調用名為progressHandler的回掉函數（可以在事件發生时值行特定的邏輯）來處理上傳進度的更新
+       //false代表事件監聽將在upload事件啟動後執行,啟動前不執行
+       //ajax.upload為XMLHttpRequest對象上傳事件,當上傳的進度值不斷更新,監聽事件也同時抓取資料進行更新
        ajax.upload.addEventListener("progress", this.progressHandler, false);
        ajax.addEventListener("load", this.completeHandler, false);
        ajax.addEventListener("error", this.errorHandler, false);
        ajax.addEventListener("abort", this.abortHandler, false);
        //把資料上傳至http://localhost:3000/upload
+       //ajax.open是XMLHttpRequest對象的方法,它接受三個值:html方法,URL,是否異步發送請求的布林值
        ajax.open("POST", "http://localhost:3000/upload");//endpoint
+       //將formdata發送到伺服器
        ajax.send(formdata);
      },
      progressHandler(event) {
